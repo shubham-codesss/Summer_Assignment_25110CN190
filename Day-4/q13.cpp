feae89e6bdf8068b9cb5505e883f08929ff49fd7
arti722457-lab <arti722457@gmail.com>
2026-06-04 17:25:58 +0530

    q13 uploaded

---
+++ Day-4/q13.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n;
+    cout << "Enter number of terms: ";
+    cin >> n;
+
+    int a = 0, b = 1;
+
+    for (int i = 0; i < n; i++) {
+        cout << a << " ";
+        int next = a + b;
+        a = b;
+        b = next;
+    }
+
+    return 0;
+}
