35e2bff013e0f0cde39cd3754f1cc9effad171cb
shubham-codess <shubh.sgh1@gmail.com>
2026-06-15 19:19:01 +0530

    Programs uploaded

---
+++ Day-15/q60.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int n;
+    cin >> n;
+
+    int arr[n];
+    int j = 0;
+
+    for(int i = 0; i < n; i++)
+        cin >> arr[i];
+
+    for(int i = 0; i < n; i++)
+    {
+        if(arr[i] != 0)
+        {
+            swap(arr[i], arr[j]);
+            j++;
+        }
+    }
+
+    for(int i = 0; i < n; i++)
+        cout << arr[i] << " ";
+
+    return 0;
+}
